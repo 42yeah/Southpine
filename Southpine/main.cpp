e1e5500fac4949613f402dbd6238fc621d7eb201
42yeah <potion@live.cn>
2019-02-02 12:42:02 +0800

    Initial Commit

---
+++ Southpine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Southpine
+//
+//  Created by 周昊 on 2019/2/2.
+//  Copyright © 2019 aiofwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
